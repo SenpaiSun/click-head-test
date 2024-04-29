bdaf59ec208adf000003e4580f626b548d2bcede
SenpaiSun <www.maxxi@mail.ru>
2024-04-29 19:39:48 +0600

    feat: add logo nav

--- src/ui/navigationHeader/NavigationHeader.tsx
+++ src/ui/navigationHeader/NavigationHeader.tsx
@@ -1,4 +1,6 @@
 'use client'
+import CartLogo from '@/public/cart-logo'
+import ExitLogo from '@/public/exit-logo'
 import { useActions } from '@/src/hooks/action'
 import { useAppSelector } from '@/src/hooks/redux'
 import Link from 'next/link'
@@ -14,7 +16,7 @@ export const NavigationHeader: React.FC = () => {
   }
 
   useEffect(() => {
-    if(!localStorage.getItem('balance')){
+    if (!localStorage.getItem('balance')) {
       localStorage.setItem('balance', '{"usd": 1000, "coins": 1000}')
     }
     const usd = localStorage.getItem('balance') ? JSON.parse(localStorage.getItem('balance') || '[]').usd : 0
@@ -31,8 +33,9 @@ export const NavigationHeader: React.FC = () => {
 
   return (
     <nav className='flex space-x-4 w-max'>
-      <Link href={'/cart/products'} className='flex flex-row gap-1 space-x-0 text-center hover:opacity-70 color-hover-text'>
+      <Link href={'/cart/products'} className='flex flex-row gap-1 space-x-0 text-center hover:opacity-70'>
         <h1>Cart</h1>
+        <CartLogo />
         <div className='flex justify-center items-center w-5 h-5 bg-red-500 rounded-full text-xs'>
           <p className='m-auto'>{countToCartProducts}</p>
         </div>
@@ -41,12 +44,15 @@ export const NavigationHeader: React.FC = () => {
         <div className='flex flex-row min-w-24 w-max border border-slate-600 rounded-2xl content-center items-center p-1'>
           <p className='h-full text-xs w-max border-r border-slate-600 pl-1 pr-1'>{balance.usd} $</p>
           <p className='h-full text-xs w-max border-r border-slate-600 pl-1 pr-1 mr-1'>{balance.coins} Coins</p>
-          <button onClick={addUsd} className='h-full text-xs w-max pl-1 pr-1 hover:bg-slate-800 rounded'>+</button>
+          <button onClick={addUsd} className='h-full text-xs w-max pl-1 pr-1 hover:bg-slate-800 rounded'>
+            +
+          </button>
         </div>
       </div>
-      <Link href={'/'} className='space-x-0 text-center hover:opacity-70 color-hover-text'>
-        Exit
-      </Link>
+        <Link href={'/'} className='flex flex-row gap-1 items-center space-x-0 text-center hover:opacity-70'>
+          <p>Exit</p>
+          <ExitLogo />
+        </Link>
     </nav>
   )
 }
